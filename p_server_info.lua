f5aab863071ef6f5bcbeefa6c2adfe2a8f09e75a
rifleh700 <rifleh700@protonmail.ch>
2025-08-27 21:58:16 +0400

    cpu + memory

--- p_server_info.lua
+++ p_server_info.lua
@@ -1,5 +1,14 @@
 local INTERVAL = 10000
 
+local function format_bytes(v)
+
+	local postfix = string.sub(v, -3)
+	if postfix == " KB" then return tonumber(string.sub(v, 1, -4)) * 1024 end
+	if postfix == " MB" then return tonumber(string.sub(v, 1, -4)) * 1024 * 1024 end
+
+	return v
+end
+
 local function registerServerInfo(stats)
 
 	setGaugeValue("server_info", 1, { platform = stats[1][2], version = stats[2][2], min_client_version = stats[1][6] })
@@ -31,6 +40,41 @@ local function collectPlayers(stats)
 	return true
 end
 
+local function collectCpuUsage(stats)
+
+	local usage, usage_avg = string.match(stats[6][2], "([%d%.]+)%% %(Avg: ([%d%.]+)%%%)")
+	setGaugeValue("server_logic_thread_cpu_usage", usage)
+	setGaugeValue("server_logic_thread_cpu_usage_avg", usage_avg)
+
+	usage, usage_avg = string.match(stats[7][2], "([%d%.]+)%% %(Avg: ([%d%.]+)%%%)")
+	setGaugeValue("server_sync_thread_cpu_usage", usage)
+	setGaugeValue("server_sync_thread_cpu_usage_avg", usage_avg)
+
+	usage, usage_avg = string.match(stats[8][2], "([%d%.]+)%% %(Avg: ([%d%.]+)%%%)")
+	setGaugeValue("server_raknet_thread_cpu_usage", usage)
+	setGaugeValue("server_raknet_thread_cpu_usage_avg", usage_avg)
+
+	return true
+end
+
+local function collectMemoryUsage(stats)
+
+	local memory = string.match(stats[5][2], "VM:%d+ MB  RSS:(%d+) MB")
+	if not memory then memory = string.match(stats[5][2], "(%d+) MB") end
+	setGaugeValue("server_memory_usage_bytes", tonumber(memory) * 1024 * 1024)
+	return true
+end
+
+local function collectNetwork(stats)
+
+	setGaugeValue("server_network_incoming_bytes", format_bytes(stats[3][4]))
+	setGaugeValue("server_network_outgoing_bytes", format_bytes(stats[4][4]))
+	setGaugeValue("server_network_incoming_packets", stats[5][4])
+	setGaugeValue("server_network_outgoing_packets", stats[6][4])
+	setGaugeValue("server_network_outgoing_packets_loss", string.sub(stats[7][4], 1, -3))
+	return true
+end
+
 local function registerAllServerInfo()
 
 	local columns, stats = getPerformanceStats("Server info")
@@ -44,6 +88,9 @@ local function collectAllServerInfo()
 	local columns, stats = getPerformanceStats("Server info")
 	collectServerFps(stats)
 	collectPlayers(stats)
+	collectCpuUsage(stats)
+	collectMemoryUsage(stats)
+	collectNetwork(stats)
 	return true
 end
 
